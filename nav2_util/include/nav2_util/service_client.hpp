729fe90c1496ef196dae08fe293fb831c8dac4fa
bpwilcox <bpwilcox@eng.ucsd.edu>
2019-09-30 11:38:14 -0700

    change std::chrono::nanoseconds::max() to std::chrono::nanoseconds(-1)

--- nav2_util/include/nav2_util/service_client.hpp
+++ nav2_util/include/nav2_util/service_client.hpp
@@ -52,7 +52,7 @@ public:
 
   typename ResponseType::SharedPtr invoke(
     typename RequestType::SharedPtr & request,
-    const std::chrono::nanoseconds timeout = std::chrono::nanoseconds::max())
+    const std::chrono::nanoseconds timeout = std::chrono::nanoseconds(-1))
   {
     while (!client_->wait_for_service(std::chrono::seconds(1))) {
       if (!rclcpp::ok()) {
@@ -105,7 +105,7 @@ public:
     return response.get();
   }
 
-  void wait_for_service(const std::chrono::nanoseconds timeout = std::chrono::nanoseconds::max())
+  void wait_for_service(const std::chrono::nanoseconds timeout = std::chrono::nanoseconds(-1))
   {
     auto sleep_dur = std::chrono::milliseconds(10);
     while (!client_->wait_for_service(timeout)) {
